b757f1ad65e56ebe64ca598f82f8c092bfdba336
bryangaytan20 <bryangaytan20@gmail.com>
2024-07-11 13:36:02 -0600

    Se crearon los primeros componentes, cabecera, inicio y pie de pagina

--- src/components/CabeceraLink/CabeceraLink.js
+++ src/components/CabeceraLink/CabeceraLink.js
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom"
 import styles from "./CabeceraLink.module.css"
+
+
 function CabeceraLink({url,children}){
     return(
         <Link to={url} className={styles.link}>
